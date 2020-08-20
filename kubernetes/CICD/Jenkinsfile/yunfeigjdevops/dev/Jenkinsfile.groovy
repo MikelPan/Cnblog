14b8bc3f512bb30e9f7cf7182814b85cc9cf8d5a
MikelPan <plyx_46204@126.com>
2020-08-21 07:50:19 +0800

    add docker pipeliene

--- kubernetes/CICD/Jenkinsfile/yunfeigjdevops/dev/Jenkinsfile.groovy
+++ kubernetes/CICD/Jenkinsfile/yunfeigjdevops/dev/Jenkinsfile.groovy
@@ -4,16 +4,12 @@ pipeline {
 
            label 'mvn-1'
            image: 'registry.jt7t.cn/maven:3.6.3-jdk-8-slim'
-           args: '  -v /root/.m2:/root/.m2 \
-                    -v /var/run/docker.sock:/var/run/docker.sock
-                '
+           args: '-v /root/.m2:/root/.m2 -v /var/run/docker.sock:/var/run/docker.sock'
        }
        docker {
            label 'mvn-2'
            image: 'registry.jt7t.cn/maven:3.6.3-jdk-8-slim'
-           args: '  -v /root/.m2:/root/.m2 \
-                    -v /var/run/docker.sock:/var/run/docker.sock
-                '
+           args: '-v /root/.m2:/root/.m2 -v /var/run/docker.sock:/var/run/docker.sock'
        }
    }
 
