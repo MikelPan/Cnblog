fb4ddc2bd2590eb08dc7218b74bf947c68b7d572
MikelPan <plyx_46204@126.com>
2020-08-21 07:53:41 +0800

    add docker pipeliene

--- kubernetes/CICD/Jenkinsfile/yunfeigjdevops/dev/Jenkinsfile.groovy
+++ kubernetes/CICD/Jenkinsfile/yunfeigjdevops/dev/Jenkinsfile.groovy
@@ -1,28 +1,22 @@
 pipeline {
-   agent {
-       docker {
+    agent any
 
-           label 'mvn-1'
-           image: 'registry.jt7t.cn/maven:3.6.3-jdk-8-slim'
-           args: '-v /root/.m2:/root/.m2 -v /var/run/docker.sock:/var/run/docker.sock'
-       }
-       docker {
-           label 'mvn-2'
-           image: 'registry.jt7t.cn/maven:3.6.3-jdk-8-slim'
-           args: '-v /root/.m2:/root/.m2 -v /var/run/docker.sock:/var/run/docker.sock'
-       }
-   }
-
-   stages {
+    stages {
     //   options { retry(2) }
-       stage('Test') {
-         when {
-           branch 'dev'
-         }
-         agent label {'mvn-1'}
-         steps {
-             sh 'mvn clean package -Dmaven.test.skip=true'
-         }
+        stage('Test') {
+            when {
+                branch 'dev'
+            }
+            agent {
+                docker {
+                    label 'mvn-1'
+                    image: 'registry.jt7t.cn/maven:3.6.3-jdk-8-slim'
+                    args: '-v /root/.m2:/root/.m2 -v /var/run/docker.sock:/var/run/docker.sock'
+                }
+            }
+            steps {
+                sh 'mvn clean package -Dmaven.test.skip=true'
+            }
       }
     }
 }
