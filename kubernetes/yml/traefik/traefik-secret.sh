56af3df09a1b928b8ecd16b41e1c13709f188f7f
MikelPan <plyx_46204@126.com>
2020-10-24 12:01:57 +0800

    调整仓库组织架构

--- kubernetes/yml/traefik/traefik-secret.sh
+++
@@ -1,2 +0,0 @@
-kubectl delete secret traefik-secret -n kube-system
-kubectl create secret generic traefik-secret --from-file=/root/.ssl/aliyun_access_key --from-file=/root/.ssl/aliyun_secret_key -n kube-system
