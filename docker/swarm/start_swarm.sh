b909bb459691401ba3fd03ef92e0976e2ed2e3b5
Mikel_Pan <plyx_46204@126.com>
2020-11-24 15:59:38 +0800

    添加帮助信息

--- docker/swarm/start_swarm.sh
+++ docker/swarm/start_swarm.sh
@@ -8,20 +8,22 @@ helpdoc(){
 Description:
 
     This shellscript is used to run a docker service in docker swarm
-    - Domain replace exmple domain, if enabled, please command to 'curl -v https://Domain:443'
+    - Domain Domain replace exmple domain
+    - Second if enabled, please command to 'curl -v https://Domain:443'
     - Service is to display to docker swarm, if not docker stack is install to faild
 
 Usage:
 
-    $0 -a <domain name> -b <service name> 
+    $0 -a <domain name> -b <service name> -c<second domain name>
 
 Option:
     -a    domain name
     -b    service name
+    -c    second name
 EOF
 }
 
-while getopts "a:b:h:" opts
+while getopts "a:b:c:h:" opts
 do
   case $opts in
     a)
@@ -31,6 +33,10 @@ do
     b)
       #SERVICE_NAME:服务名称
       SERVICE_NAME=$OPTARG
+      ;;
+    c)
+      #SECOND_DOMAIN:二级域名
+      SECOND_DOMAIN=$OPTARG
       ;;
     h)
       helpdoc
