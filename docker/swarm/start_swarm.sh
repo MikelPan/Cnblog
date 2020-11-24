3646bdc59e6ea1d1c802b20bb5c6878999e2b0b1
Mikel_Pan <plyx_46204@126.com>
2020-11-24 15:26:54 +0800

    添加swarm服务部署脚本

---
+++ docker/swarm/start_swarm.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+BASE_DIR=$(cd "$(dirname "$0")";pwd)
+cd $BASE_DIR
+while getopts "a:b:h" opts
+do
+  case $opts
+    a)
+      #DOMAIN:域名
+      DOMAIN=$OPTARG
+      ;;
+    b)
+      #SERVICE_NAME:服务名称
+      SERVICE_NAME=$OPTARG
+      ;;
+    h)
+      echo -e "OPTTIONS:\n-a:域名(必选) \n-b:服务名(必选)"
+      exit 1
+      ;;
+    ?)
+      echo "missing options,pls check!"
+      exit 1
+      ;;
+  esac
+done
+
+#可选参数赋值
+
+# 安装filebrowers
+Install_filebrower {
+  sed -i 's/local.cluster/${DOMAIN}/g' docker-compose-${SERVICE_NAME}.yml
+  docker stack deploy -c docker-compose-${SERVICE_NAME}.yaml ${SERVICE_NAME}
+}
+
+main {
+  Install_filebrower
+}
+
+main
