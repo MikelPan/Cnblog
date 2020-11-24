9f204b50730f602ed7e4932806f895b92480ffa8
Mikel_Pan <plyx_46204@126.com>
2020-11-24 16:46:18 +0800

    添加帮助信息

--- docker/swarm/start_swarm.sh
+++ docker/swarm/start_swarm.sh
@@ -63,7 +63,6 @@ fi
 
 # 安装filebrowers
 Install_filebrower {
-  cp docker-compose-template.yml docker-compose-${SERVICE_NAME}.yml
   sed -i "s/local.cluster/${DOMAIN}/g" docker-compose-${SERVICE_NAME}.yml
   sed -i "s/`second.local.cluster`/`${SECOND_DOMAIN}`/g" docker-compose-${SERVICE_NAME}.yml
   docker stack deploy -c docker-compose-${SERVICE_NAME}.yml ${SERVICE_NAME}
