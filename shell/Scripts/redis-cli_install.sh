f2facbfa87647f1d3ca176884806532643d0e691
MikelPan <plyx_46204@126.com>
2021-03-07 17:52:41 +0800

    修改redis安装脚本

--- shell/Scripts/redis-cli_install.sh
+++ shell/Scripts/redis-cli_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 wget https://download.redis.io/releases/redis-5.0.10.tar.gz -P /usr/local/src 
-tar zxvf /usr/local/src/redis-5.0.10.tar.gz
+tar zxvf /usr/local/src/redis-5.0.10.tar.gz -C /usr/local/src
 cd /usr/local/src/redis-5.0.10
 make
 cp /usr/local/src/redis-5.0.10/src/redis-cli /usr/bin/redis-cli
