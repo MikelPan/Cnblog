b5644f3c38b454bba445eef07dcd88a9c282c11d
MikelPan <plyx_46204@126.com>
2021-03-07 18:01:38 +0800

    修改mysql client安装

--- shell/Scripts/mysql-client_install.sh
+++ shell/Scripts/mysql-client_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 rpm_url="https://mirrors.tuna.tsinghua.edu.cn/mysql/yum/mysql57-community-el7/"
-for pkg in (mysql-community-common-5.7.33-1.el7.x86_64.rpm mysql-community-libs-5.7.33-1.el7.x86_64.rpm mysql-community-client-5.7.33-1.el7.x86_64.rpm)
+for pkg in mysql-community-common-5.7.33-1.el7.x86_64.rpm mysql-community-libs-5.7.33-1.el7.x86_64.rpm mysql-community-client-5.7.33-1.el7.x86_64.rpm
 do
     wget $rpm_url$pkg -P /usr/local/src
     rpm -ivh /usr/local/src/$pkg
