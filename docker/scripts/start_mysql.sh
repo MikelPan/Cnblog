c21717e96ed7e0789db54de75fa71d1c532b1f95
MikelPan <plyx_46204@126.com>
2021-04-05 09:30:28 +0800

    添加docker mysql启动脚本

--- docker/scripts/start_mysql.sh
+++ docker/scripts/start_mysql.sh
@@ -5,7 +5,6 @@ docker rm mysql-server
 docker run --name mysql-server \
   -p 3306:3306 \
   -e MYSQL_ROOT_PASSWORD=123456 \
-  #-v /etc/mysql/my.cnf:/etc/mysql/conf.d/my.cnf \
   -v /var/lib/mysql:/var/lib/mysql \
   --restart always \
   -d mysql:5.7.33 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci 
