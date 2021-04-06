d5b63b95032e178378a11842174ae996727934d0
MikelPan <plyx_46204@126.com>
2021-04-06 20:46:12 +0800

    添加docker启动rabbitmq

---
+++ docker/scripts/start_rabbitmq.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+docker stop rabbitmq-server
+docker rm rabbitmq-server
+docker run --name rabbitmq-server --restart=alwsys \
+  -p 5672:5672 \
+  -p 15672:15672 \
+  -v /var/lib/rabbitmq:/var/lib/rabbitmq \
+  -v /etc/rabbitmq/rabbitmq.conf:/etc/rabbitmq/rabbitmq.conf \
+  --hostname hostname \
+  -e RABBITMQ_DEFAULT_VHOST=hostname \
+  -e RABBITMQ_DEFAULT_USER=admin \
+  -e RABBITMQ_DEFAULT_PASS=ddddddd \
+  -d rabbitmq:3.8.14-management
