6e8124572004f80c7f94fa5468b48516ee89f5c2
MikelPan <plyx_46204@126.com>
2021-07-08 06:51:28 +0800

    添加redis目录读写权限

--- docker/scripts/docker_redis.sh
+++ docker/scripts/docker_redis.sh
@@ -3,7 +3,7 @@
 docker stop redis
 docker rm redis
 [ -d "/data/redis" ] || mkdir -pv /data/redis
-chmod -R 1001:1001 /data/redis
+chown -R 1001:1001 /data/redis
 docker run --name redis \
     -e ALLOW_EMPTY_PASSWORD=yes \
     -v /data/redis:/bitnami/redis/data \
