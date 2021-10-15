5ff371031e6202f871dcd7995a650a25bea836bf
MikelPan <plyx_46204@126.com>
2021-10-15 17:47:45 +0800

    添加oom检查脚本

---
+++ shell/Scripts/oom.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+for proc in $(find /proc -maxdepth 1 -regrx '/proc/[0-9]+')
+do
+    printf "%2d %5d %s\n" \
+        "$(cat $proc/oom_sorce)" \
+        "$(basename $proc)" \
+        "$(cat $proc/cmdline | tr '\0' ''|head -c 50)"
+done 2>/dev/null | sort -rn | head -n 10
