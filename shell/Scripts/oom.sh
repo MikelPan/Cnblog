1bc450f7e300d253ef5fcff3ad7f0ba053bbd8fb
MikelPan <plyx_46204@126.com>
2021-10-15 18:26:30 +0800

    添加oom检查脚本

--- shell/Scripts/oom.sh
+++ shell/Scripts/oom.sh
@@ -6,5 +6,5 @@ do
     printf "%2d %5d %s\n" \
         "$(cat $proc/oom_sorce)" \
         "$(basename $proc)" \
-        "$(cat $proc/cmdline |head -c 50)" \
+        "$(cat $proc/cmdline |head -c 50)"
 done 2>/dev/null | sort -rn | head -n 10
