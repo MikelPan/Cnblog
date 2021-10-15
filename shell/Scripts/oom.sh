7f8097ab8e3657496085d441e7c7613fcde9a03f
MikelPan <plyx_46204@126.com>
2021-10-15 18:28:31 +0800

    添加oom检查脚本

--- shell/Scripts/oom.sh
+++ shell/Scripts/oom.sh
@@ -4,7 +4,7 @@ for proc in $(find /proc -maxdepth 1 -regex '/proc/[0-9]+')
 do
     echo $proc
     printf "%2d %5d %s\n" \
-        "$(cat $proc/oom_sorce)" \
+        "$(cat $proc/oom_score)" \
         "$(basename $proc)" \
         "$(cat $proc/cmdline |head -c 50)"
-done | sort -rn | head -n 10
+done 2>/dev/null| sort -rn | head -n 10
