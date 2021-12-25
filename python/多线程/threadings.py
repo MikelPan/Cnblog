7866a3986a3c8cc9c9462bdf3607f010b30c87a5
MikelPan <plyx_46204@126.com>
2021-12-25 08:49:05 +0800

    调整仓库

--- python/多线程/threadings.py
+++ python/多线程/threadings.py
@@ -4,7 +4,7 @@ import time
 
 
 def thread_func():  # 线程函数
-    # time.sleep(1)
+    time.sleep(1)
     i = 0
     while(1):
         print(datetime.now())
@@ -19,8 +19,8 @@ def many_thread():
         t.setDaemon(True)  # 给每个子线程添加守护线程
     for t in threads:  # 循环启动500个线程
         t.start()
-    # for t in threads:
-    #     t.join(2)  # 设置子线程超时2秒
+    for t in threads:
+        t.join(2)  # 设置子线程超时2秒
 
 
 if __name__ == '__main__':
