3c4a2b06061a244102142dbeb526a1aa71f762bf
MikelPan <plyx_46204@126.com>
2021-12-28 15:32:46 +0800

    添加io监控

--- shell/Scripts/shell-template/disk.sh
+++ shell/Scripts/shell-template/disk.sh
@@ -22,7 +22,7 @@ CYAN='\033[38;5;6m'
 #########################
 
 main() {
-    device_num=`iostat -x|grep "^sd[a-z]"|wc-l`
+    device_num=`iostat -x|grep "^sd[a-z]"|wc -l`
     echo -e "${CYAN} =========================磁盘使用情况==========================${REST}"
     awk 'BEGIN {print "device\tavgqu-sz"}'  
     iostat -x 1 3|egrep "^sd[a-z]"|tail -n +$((device_num+1))|awk '{io_long[$1]+=$9}END{for (i in io_long) print i"\t",io_long[i]}'
