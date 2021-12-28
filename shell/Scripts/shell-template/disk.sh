7a49b8c40c7023074ebd1f6cd9e239d026fde4c4
MikelPan <plyx_46204@126.com>
2021-12-28 15:29:36 +0800

    添加io监控

---
+++ shell/Scripts/shell-template/disk.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#Description: 磁盘使用情况统计脚本
+#Author: mikelLam
+#Created Time: 2021/12/28 15:20
+
+# Constants
+RESET='\033[0m'
+RED='\033[38;5;1m'
+GREEN='\033[38;5;2m'
+YELLOW='\033[38;5;3m'
+MAGENTA='\033[38;5;5m'
+CYAN='\033[38;5;6m'
+
+# Function
+
+########################
+# Print to STDERR
+# Arguments:
+#   Missing to print
+# Returns:
+#   None
+#########################
+
+main() {
+    device_num=`iostat -x|grep "^sd[a-z]"|wc-l`
+    echo -e "${CYAN} =========================磁盘使用情况==========================${REST}"
+    awk 'BEGIN {print "device\tavgqu-sz"}'  
+    iostat -x 1 3|egrep "^sd[a-z]"|tail -n +$((device_num+1))|awk '{io_long[$1]+=$9}END{for (i in io_long) print i"\t",io_long[i]",i}'
+}
+
+main "$@"
